a1011bdc22a25104333c7702b17a2cbab6e916c9
roberto_arquivei <roberto.jurnio@arquivei.com.br>
2021-12-22 20:11:58 -0300

    refactored with (make book slice), protected with Mutex and created concurrency goroutines... I guess =P

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// TODO: Retirar o ponteiro dos autores. DONE!
-
 // Book Struct (Model)
 type Book struct {
 	ID     string `json:"id"`
@@ -28,81 +26,89 @@ type Author struct {
 }
 
 // Init books var as slice Book struct
-var books []Book
+var books = make([]Book, 0)
 var newId = 3
 var mu sync.Mutex
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	mu.Lock()
 	json.NewEncoder(w).Encode(books)
+	mu.Unlock()
 }
 
 // Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // Get params
+
 	// Loop through books and find with id
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
+	mu.Lock()
+	for _, book := range books {
+		if book.ID == params["id"] {
+			json.NewEncoder(w).Encode(book)
+			// return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
+	mu.Unlock()
+	// json.NewEncoder(w).Encode(&Book{})
 
 }
-
-// TODO: criar contador + 1 para novos livros DONE!
 
 // Create a New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	mu.Lock()
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(newId) //strconv.Itoa(rand.Intn(10000000)) // Mock ID
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-	newId++
-}
 
-// TODO: proteger as operações do slice com mutex DONE!
-// TODO: executar update sem alterar o array DONE!
+	book.ID = strconv.Itoa(newId)
+	books = append(books, book)
+	newId++
+	json.NewEncoder(w).Encode(book)
+	mu.Unlock()
+}
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			mu.Lock()
-			var book Book
+
+	mu.Lock()
+	for index, book := range books {
+		if book.ID == params["id"] {
 			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			fmt.Println(book)
 			books[index] = book
 			json.NewEncoder(w).Encode(book)
-			mu.Unlock()
-			return
+			// return
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	mu.Unlock()
+	// json.NewEncoder(w).Encode(books)
 }
 
 // Delete a Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
+
+	mu.Lock()
+	for index, book := range books {
+		if book.ID == params["id"] {
 			books = append(books[:index], books[index+1:]...)
-			break
+			fmt.Println(books)
+			json.NewEncoder(w).Encode(book)
+			// break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	mu.Unlock()
+	// json.NewEncoder(w).Encode(books)
 }
 
 func main() {
+
 	// Init Router
 	r := mux.NewRouter()
 
@@ -110,12 +116,12 @@ func main() {
 	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Goland Book", Author: Author{Firstname: "Jhon", Lastname: "Doe"}})
 	books = append(books, Book{ID: "2", Isbn: "654942", Title: "Book Two", Author: Author{Firstname: "Steve", Lastname: "Smith"}})
 
-	// Route Handlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	// Route Handlers / Endpoints / Concurrency Goroutines - This works here? I down't know =D
+	go r.HandleFunc("/api/books", getBooks).Methods("GET")
+	go r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	go r.HandleFunc("/api/books", createBook).Methods("POST")
+	go r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	go r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
